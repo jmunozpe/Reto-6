2dd92d97d1efee4f0d47966185d3d66e43d0418c
jmunozpe <jmunozpe@unal.edu.co>
2025-11-05 15:15:22 -0500

    Update reto_6_2.py

--- reto_6_2.py
+++ reto_6_2.py
@@ -1,1 +1,25 @@
 
+class InputTypeError(Exception):
+    pass
+
+def ensure_str(x, name):
+    if not isinstance(x, str):
+        raise InputTypeError(f"'{name}' must be a string. Got: {type(x).__name__}")
+
+def is_palindrome(word: str) -> bool:
+    ensure_str(word, "word")
+
+    s = word.lower()
+    i, j = 0, len(s) - 1
+    while i < j:
+        if s[i] != s[j]:
+            return False
+        i += 1
+        j -= 1
+    return True
+
+
+if __name__ == "__main__":
+    w = input("Enter a word: ")
+    print("Is palindrome?", "Yes" if is_palindrome(w) else "No")
+
